31f1f043380592a52497370aa8264f3e6ae91dde
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-22 11:29:21 -0300

    Criado botoes para trocar os fragments e adicionado logica

--- app/src/main/java/br/com/fiap/a2tina_android_fragments/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_fragments/MainActivity.java
@@ -2,12 +2,21 @@ package br.com.fiap.a2tina_android_fragments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentContainer = (FrameLayout)findViewById(R.id.fragment_container);
+    }
+
+    public void exibirCarro1(View view) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Carro1Fragment()).commit();
     }
 }
